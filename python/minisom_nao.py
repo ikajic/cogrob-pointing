f77b97bae893a50832bd95a1c8199f5fc7612040
Ivana Kajic <ivana.kajic@gmail.com>
2013-09-26 10:34:33 +0200

    Data path entered in cm

--- python/minisom_nao.py
+++ python/minisom_nao.py
@@ -1,20 +1,28 @@
+#!usr/bin/python
+
 from minisom import MiniSom
 from numpy import genfromtxt,array,linalg,zeros,mean,std,apply_along_axis
+import sys
+import os
 import pylab as plt
 import plot_som as psom
 
-base_path = '/home/ivana/knnl-0.1.4/knnl/build/make/data/'	
+nrarg = len(sys.argv)
 
-# store data in a list
-data = []
-#data.append(psom.fetch_data(base_path + 'hands'))
-#data.append(psom.fetch_data(base_path + 'joints'))
+if nrarg<2:
+	raise Exception('Missing data path')
 
+path = str(sys.argv[1])
+
+if not os.path.exists(path):
+	raise Exception('Path doesn\'t exist')
+
+# columns of interest
 hands = (3,4,5)
 joints = (9,10,11)
 		
-data = [genfromtxt('/home/ivana/babbling_KB_left_arm.dat', skiprows=3, usecols=hands)[::50],
-	genfromtxt('/home/ivana/babbling_KB_left_arm.dat', skiprows=3, usecols=joints)[::50]]
+data = [genfromtxt(path, skiprows=3, usecols=hands)[::50],
+	genfromtxt(path, skiprows=3, usecols=joints)[::50]]
 
 d = [data[0].shape[1], data[1].shape[1]]
 
