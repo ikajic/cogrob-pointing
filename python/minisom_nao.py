32df26f7052108c82936f31fdac460112d5f44b4
Ivana Kajic <ivana.kajic@gmail.com>
2013-09-27 10:35:56 +0200

    Corrected hebbian learning and additional plots

--- python/minisom_nao.py
+++ python/minisom_nao.py
@@ -1,11 +1,15 @@
-from numpy import genfromtxt, zeros, product
+from numpy import genfromtxt, zeros, product, setdiff1d, arange
 from parameters import param
 
 import plot_som as ps
 import sys
 import os
+import pdb
 
 from minisom import MiniSom
+
+#TODO
+import pylab as pl
 
 def get_path():
 	"""
@@ -60,18 +64,20 @@ def hebbian_learning(som1, som2, data):
 	s1, s2 = som1.weights.shape, som2.weights.shape
 	hebb = zeros((param['nr_rows'], param['nr_cols'], param['nr_rows'], param['nr_cols']))
 	
-	for dp1, dp2 in zip(data['hands'], data['joints'])[:10]:
+	for dp1, dp2 in zip(som1.data, som2.data):
 		act1 = som1.activate(dp1)
 		act2 = som2.activate(dp2)
+				
+		idx1 = som1.winner(dp1)#divmod(act1.argmax(), param['nr_rows'])
+		idx2 = som2.winner(dp2)#divmod(act2.argmax(), param['nr_rows'])
 		
-		idx1 = divmod(act1.argmax(), param['nr_rows'])[::-1]
-		idx2 = divmod(act2.argmax(), param['nr_rows'])[::-1]
-		
-		hebb[idx1[0], idx1[1], idx2[0], idx2[1]] = param['eta'] * act1[idx1[0], idx1[1]] * act2[idx2]
+		#pdb.set_trace()
+		#print idx1, idx2, param['eta'] * act1[idx1] * act2[idx2]
+		hebb[idx1[0], idx1[1], idx2[0], idx2[1]] += param['eta'] * act1[idx1] * act2[idx2]
 		
 	return hebb
 
-# debug
+# useful plotting, TODO extract to plot som
 def plot(som_hands, som_joints):
 	wi_0, w_0 = som_hands.get_weights()	
 	wi_1, w_1 = som_joints.get_weights()
@@ -79,8 +85,31 @@ def plot(som_hands, som_joints):
 	ps.plot_3d(final_som=w_0, data=som_hands.data[::50], init_som=wi_0, nr_nodes=param['n_to_plot'], title='SOM Hands')
 	ps.plot_3d(final_som=w_1, data=som_joints.data[::50], init_som=wi_1, nr_nodes=param['n_to_plot'], title='SOM Joints')
 
-	ps.show()
 
+def plot_inactivated_nodes(som, inact):
+	_, w = som.get_weights()	
+	act = setdiff1d(arange(w.shape[0]), inact)
+	
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection = '3d')
+	data = som.data[::50]
+	
+	if data.shape[1] > 3:
+		print "using only 3 dimensions for plotting"
+		data = data[:,:3]
+		
+	# plot data points	
+	d = ax.plot(data[:, 0], data[:,1], data[:,2], c='b', marker='*', linestyle='None', alpha=0.4, label='data')
+	
+	# plot activated nodes in green
+	act_nod = w[act, :]
+	a = ax.plot(act_nod[:, 0], act_nod[:, 1], act_nod[:, 2], c='g', marker='o', alpha = 0.6, label='neurons', markersize=4)
+	
+	#plot inactivated nodes in red
+	in_w = w[inact, :]
+	i = ax.plot(in_w[:, 0], in_w[:, 1], in_w[:, 2], c='r', marker='o', alpha = 0.6, label='inact. neurons', markersize=6, linestyle='None')
+	plt.legend(nrpoints=1)
+	
 if __name__=="__main__":
 	path = get_path()
 
@@ -91,9 +120,14 @@ if __name__=="__main__":
 	som_hands = train_som(data['hands'])
 	som_joints = train_som(data['joints'])
 
-	#plot(som_hands, som_joints)
+	plot(som_hands, som_joints)
+	inact = som_joints.activation_response(som_joints.data)
+	coord_inact = where(inact.flatten()==0)[0]
+	plot_inactivated_nodes(som_joints, coord_inact)
 	
+		
 	# hebbian learning between maps
 	hebb_weights = hebbian_learning(som_hands, som_joints, data)
+	h = hebb_weights.reshape(25, 25)
 
 	# print the strongest connections
--- python/parameters.py
+++ python/parameters.py
@@ -2,7 +2,7 @@ param = {
 	'nrpts': 50,
 	'hands': (3,4,5),
 	'joints': (9,10,11,12),
-	'nr_epochs': 10,
+	'nr_epochs': 1000,
 	'nr_rows': 5,
 	'nr_cols': 5,
 	'sigma': 0.6, 
