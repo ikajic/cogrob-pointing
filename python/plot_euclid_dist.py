f5eaa4ea2aeec8b59d5caaa9a047c9a2efd4502f
Ivana Kajic <ivana.kajic@gmail.com>
2013-11-07 17:27:35 +0100

    Saving all three babbling configs.

--- python/plot_euclid_dist.py
+++ python/plot_euclid_dist.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = np.genfromtxt('out.csv', dtype=np.float, skiprows=1, delimiter=',')
-
+b = np.genfromtxt('out_b.csv', dtype=np.float, skiprows=1, delimiter=',')
+i = np.genfromtxt('out_i.csv', dtype=np.float, skiprows=1, delimiter=',')
+a = np.genfromtxt('out_a.csv', dtype=np.float, skiprows=1, delimiter=',')
 
 # Load the knowledge base from the babbling phase
 path = '/home/ivana/babbling_KB_left_arm.dat'
 with open(path) as dat:
-	kb = genfromtxt(dat, dtype=np.float, skiprows=2, usecols=(2,3,4,7,8,9,10))
+	kb = np.genfromtxt(dat, dtype=np.float, skiprows=2, usecols=(2,3,4,7,8,9,10))
 
 # Find the closest joint positions and their distances to predicted positions
-def create_all():
-	f_all = np.zeros((len(f), 17), dtype=np.float)
-	for i, row in enumerate(f):
+def create_all(missing):
+	f_all = np.zeros((len(missing), 17), dtype=np.float)
+	for i, row in enumerate(missing):
 		hands = row[1:4]	
 		joints = row[9:13]
 		distances = map(lambda x: np.linalg.norm(x-hands), kb[:, :3])	
@@ -20,12 +21,14 @@ def create_all():
 		f_all[i, :] = np.r_[row[:8], 
 							kb[idx, 3:], 
 							joints, 
-							np.linalg.norm(true_joint-joints)]
+							np.linalg.norm(kb[idx, 3:]-joints)]
+							
+	return f_all
 
-b = f[np.where(f[:,0]==0), :][0]
-i = f[np.where(f[:,0]==1), :][0]
-a = f[np.where(f[:,0]==2), :][0]
-
+#b = create_all(b)
+#a = create_all(a)
+#i = create_all(i)
+	
 for idx, lab in zip([7, 16], ['hands', 'joints']):
 
 	# hands column 7
@@ -48,8 +51,6 @@ for idx, lab in zip([7, 16], ['hands', 'joints']):
 
 	plt.plot(i[:, idx], color='g', label='int')
 	plt.plot(avg_i, color='g', alpha=0.3)
-	plt.ylim([0,300])
-	plt.xlim([0,250])
 
 	plt.xlabel('Time [0.5 s]')
 	plt.legend()
