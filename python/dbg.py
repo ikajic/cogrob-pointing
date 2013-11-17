7ba3d865971223bb03f572e8814b7dc1f3f74648
Ivana <ivana.kajic@gmail.com>
2013-11-17 20:03:50 +0100

    Added dbg script. Support for t-test in progress.

---
+++ python/dbg.py
@@ -0,0 +1,71 @@
+import plot_som as ps
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_and_save(som_hands, som_joints, savepath='', param=None, offset=50):
+    """
+    3D Plots of babblilng data and SOM sheets. If neurons have more than 3
+    dimensions they are truncated to 3
+    """
+     
+    wi_0, w_0 = som_hands.get_weights()    
+    wi_1, w_1 = som_joints.get_weights()
+
+    ps.plot_3d(final_som=w_0, data=som_hands.data[::offset, :], init_som=wi_0, nr_nodes=param['n_to_plot'], title='SOM Hands')
+    if savepath: plt.savefig(savepath + 'som_hands.png')
+    
+    ps.plot_3d(final_som=w_1, data=som_joints.data[::offset, :], init_som=wi_1, nr_nodes=param['n_to_plot'], title='SOM Joints')
+    if savepath: plt.savefig(savepath + 'som_joints.png')
+
+
+def plot_inactivated_nodes(som, inact):
+    """
+    For a given SOM plots neurons and marks the ones which are never
+    activated in green
+    """
+    _, w = som.get_weights()    
+    act = np.setdiff1d(np.arange(w.shape[0]), inact)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    data = som.data
+    
+    # Just a fraction of data used for plotting
+    data = data[:,:3][::20]
+    d = ax.plot(data[:, 0], data[:,1], data[:,2], c='b', marker='*', linestyle='None', alpha=0.4, label='data')
+    
+    # Plot activated nodes in green
+    act_nod = w[act, :]
+    a = ax.plot(act_nod[:, 0], act_nod[:, 1], act_nod[:, 2], c='g', marker='o', alpha = 0.6, label='neurons', markersize=4)
+    
+    # Plot inactivated nodes in red
+    in_w = w[inact, :]
+    i = ax.plot(in_w[:, 0], in_w[:, 1], in_w[:, 2], c='r', marker='o', alpha = 0.6, label='inact. neurons', markersize=6, linestyle='None')
+    plt.legend(numpoints=1)
+    
+def print_strongest_connections(hebb_weights):
+    # print the strongest connections
+    for i in xrange(param['nr_rows']):
+        for j in xrange(param['nr_cols']):
+            maxw = -100
+            map2X = 0; 
+            map2Y = 0;
+            for k in xrange(param['nr_rows']):
+                for t in xrange(param['nr_cols']):
+                    if (hebb_weights[i][j][k][t] > maxw):
+                        maxw= hebb_weights[i][j][k][t];
+                        map2X = k; map2Y = t;
+            string = "(" + str(map2X) + ", " + str(map2Y) + ")  "
+            sys.stdout.write(string)
+        print ''    
+        
+
+def dbg_print_inact(som_hands, som_joints, defstream):
+    inact = som_hands.activation_response(som_hands.data)
+    coord_inact = np.where(inact.flatten()==0)[0]
+    defstream.write("Hands: %.0f%% of unactivated nodes\n"%(len(coord_inact)/np.product(som_hands.weights.shape[:2]) *100))
+    
+    inact = som_joints.activation_response(som_joints.data)
+    coord_inact = np.where(inact.flatten()==0)[0]
+    defstream.write("Joints: %.0f%% of unactivated nodes\n"%(len(coord_inact)/np.product(som_joints.weights.shape[:2]) *100))
--- python/parameters.py
+++ python/parameters.py
@@ -2,11 +2,11 @@ param = {
 	'nrpts': 50,
 	'hands': (2,3,4),
 	'joints': (7,8,9,10),
-	'nr_epochs': 50,
-	'nr_rows': 7,
-	'nr_cols': 7,
+	'nr_epochs': 10000,
+	'nr_rows': 15,
+	'nr_cols': 15,
 	'sigma': 0.4, 
-	'learning_rate': 0.7,
+	'learning_rate': 0.3,
 	'n_to_plot': 400,
 	'eta': 0.01
 	} 
