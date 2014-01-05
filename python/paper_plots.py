9f5e0ce84879cab9334db6a7211cc1d14cde0d98
Ivana <ivana.kajic@gmail.com>
2014-01-05 11:00:15 +0100

    Added plots.

--- python/paper_plots.py
+++ python/paper_plots.py
@@ -6,15 +6,18 @@ import numpy as np
 
 # read babbling data
 dpath = '../data/r_37min_74k.dat'
+spath = './plots/'
 data = read_data(dpath, 100, 'u')
 
 # read SOM configurations
 n=1
 hands_small = fetch_som('somconf/5x5_20000_u100/')
 plot_3d(hands_small.weights[::n], data['hands'], title='')
+plt.savefig(spath+'SOM_5x5.png')
 
 hands_big = fetch_som('somconf/15x15_20000_u100/')
 plot_3d(hands_big.weights[::n], data['hands'], title='')
+plt.savefig(spath+'SOM_15x15.png')
 
 show()
 
@@ -30,8 +33,8 @@ plt.xlim([0, 1])
 plt.grid()
 plt.xlabel('Time [s]')
 plt.ylabel('Horizontal axis distance [mm]')
-
 plt.legend()
+plt.savefig(spath+'trajectory_15x15.png')
 
 # smaller network
 exp_data = np.genfromtxt('../experiments/nao/bigger_map_100ms/out_s100.csv', dtype=np.float, usecols=(1,4), skip_header=1, delimiter=',')
@@ -44,5 +47,5 @@ plt.xlim([0, 1])
 plt.grid()
 plt.xlabel('Time [s]')
 plt.ylabel('Horizontal axis distance [mm]')
-
 plt.legend()
+plt.savefig(spath+'trajectory_5x5.png')
